3718c5066959811132b361d25775b1935238d7a6
Brayan Torres <47356178+BrayanTorres2@users.noreply.github.com>
2022-03-21 22:07:56 -0500

    Add files via upload

---
+++ M2 L2 E1/entradas_caja_negra_salidas.py
@@ -0,0 +1,9 @@
+#Entradas
+a=int(input("Digite a: "))
+b=int(input("Digite b: "))
+#CAJA NEGRA
+suma=a+b
+#Salida
+print(f"La suma es: {suma}")
+#print("La suma es: ",suma)
+#print("La suma es: "+str(suma))
---
+++ M2 L2 E1/entradas_salidas.py
@@ -0,0 +1,9 @@
+#entradas
+numero1=int(input("Digite numero 1: "))
+numero2=int(input("Digite numero 1: "))
+#caja negra
+suma=numero1+numero1
+#Salidas
+print("Suma: ", suma)
+print("Suma: "+str(suma))
+print(f"Suma: {suma}")
